3c2e170f83b1bec55c84cf9edb61ceeea9b953fc
Max Verbeek <m4xv3rb33k@gmail.com>
2022-01-30 16:22:11 +0100

    feat: add mozilla shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,14 @@
 {pkgs ? import <nixpkgs> {}}:
-
-pkgs.mkShell rec {
-  propagatedBuildInputs = with pkgs; [
-    cargo cargo-edit rustc rustfmt dbus pkgconfig
-  ];
-}
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    buildInputs = [
+      # to use the latest stable:
+      nixpkgs.latest.rustChannels.stable.rust
+      cargo-edit rustfmt dbus pkgconfig
+    ];
+  }
